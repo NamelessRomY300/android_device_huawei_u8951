46e57375ed5d1a29349d0406afa66ce747063362
Y300-0100 <bosko.brajovich@gmail.com>
2014-09-30 00:42:52 +0200

    I am lost :-)

--- nfc/nfc.c
+++ nfc/nfc.c
@@ -16,8 +16,8 @@
 #include <errno.h>
 #include <string.h>
 
-#include <hardware/libhardware/include/hardware.h>
-#include <hardware/libhardware/include/nfc.h>
+#include <hardware/hardware.h>
+#include <hardware/nfc.h>
 
 static uint8_t pn544_eedata_settings[][4] = {
     // DIFFERENTIAL_ANTENNA
